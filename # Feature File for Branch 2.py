082a398b04bc310eba1e6dfb773d2f18dc2e19c2
adrien <adrien.chiantalassa@gmail.com>
2024-12-10 14:04:14 +0100

    hello

---
+++ # Feature File for Branch 2.py
@@ -0,0 +1,8 @@
+# Feature File for Branch 2
+
+def feature_branch2():
+    """Function specific to Branch 2."""
+    return "This is a feature exclusive to Branch 2."
+
+if __name__ == "__main__":
+    print(feature_branch2())
